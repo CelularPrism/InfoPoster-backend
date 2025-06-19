53aba62d94eff4df40db8f2edee38edb851f59e8
Konstantin Kivishev <46044765+CelularPrism@users.noreply.github.com>
2025-06-19 11:23:16 +0700

    add category model for application

---
+++ Models/Administration/ApplicationCategoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InfoPoster_backend.Models.Administration
+{
+    public class ApplicationCategoryModel
+    {
+        public Guid Id { get; set; }
+        public Guid ApplicationId { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid SubcategoryId { get; set; }
+    }
+}
