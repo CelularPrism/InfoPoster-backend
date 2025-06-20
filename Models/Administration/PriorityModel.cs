56ab6dd6bb58e1af0636c6703fce350231a4f38e
Konstantin Kivishev <46044765+CelularPrism@users.noreply.github.com>
2025-06-20 10:32:36 +0700

    add popularity model

---
+++ Models/Administration/PriorityModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InfoPoster_backend.Models.Administration
+{
+    public class PopularityModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ApplicationId { get; set; }
+        public Guid CategoryId { get; set; }
+        public Guid SubcategoryId { get; set; }
+        public int Popularity { get; set; }
+    }
+}
