19ee1e3039708f9a6c2d554ec35a0517706eb968
Konstantin Kivishev <46044765+CelularPrism@users.noreply.github.com>
2025-07-09 12:19:22 +0700

    update request for published organization

--- Handlers/Administration/Organization/GetPublishedOrganizationHandler.cs
+++ Handlers/Administration/Organization/GetPublishedOrganizationHandler.cs
@@ -4,7 +4,10 @@ using MediatR;
 
 namespace InfoPoster_backend.Handlers.Administration.Organization
 {
-    public class GetPublishedOrganizationRequest : IRequest<List<OrganizationModel>> { }
+    public class GetPublishedOrganizationRequest : IRequest<List<OrganizationModel>>
+    {
+        public string SearchText { get; set; }
+    }
 
     public class GetPublishedOrganizationHandler : IRequestHandler<GetPublishedOrganizationRequest, List<OrganizationModel>>
     {
@@ -20,7 +23,7 @@ namespace InfoPoster_backend.Handlers.Administration.Organization
             var organizations = await _repository.GetOrganizationList();
             var popularity = await _repository.GetPopularityList(Models.Administration.POPULARITY_PLACE.MAIN);
 
-            var result = organizations.Where(o => !popularity.Select(p => p.ApplicationId).Contains(o.Id)).ToList();
+            var result = organizations.Where(o => !popularity.Select(p => p.ApplicationId).Contains(o.Id) && o.Name.Contains(request.SearchText)).ToList();
             return result;
         }
     }
